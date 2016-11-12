bf6a7c5728b0157958edbb048a37e4b00aecacdd
PalashTanejaPro <tanejapalash@gmail.com>
2016-11-12 14:43:28 +0530

    Create macros.cpp
    Added new file with common macros for fast programming

---
+++ macros.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector< pii > vpi;
+#define s(a) sort((a).begin(),(a).end())
+#define FOR(i,a,n) for(int i=a;i<n;++i)
+#define forall(i,a,n) for(int i=a;i<=n;++i)
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+#define sz(a) int((a).size())
+#define all(c) (a).begin(),(a).end()
+#define char2int(c) (c-'0')
+#define IOS ios_base::sync_with_stdio(0);
+#define printA(a,n) forall(i,0,n) cout<<a[i]<<(i==R?'\n':" ")
+#define printV(a) printA(a,v.size())
+#define MOD 1000000007
+#define MAXN 100005
+
+int main(){
+	
+}
